5096d5362dc66173b5b85e39f41dc2233b08e739
r0nan12 <roman-skrupun@yandex.ru>
2016-11-10 20:17:33 +0200

    with true migrations

--- db/migrate/20161028153753_remove_author_id_from_coments.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAuthorIdFromComents < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :coments, :author_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107032439) do
+ActiveRecord::Schema.define(version: 20161110161533) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,15 +21,6 @@ ActiveRecord::Schema.define(version: 20161107032439) do
     t.integer  "total_coments"
     t.datetime "create_date"
     t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
-  create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "surname"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_authors_on_article_id"
   end
 
   create_table "coments", force: :cascade do |t|
@@ -62,6 +53,7 @@ ActiveRecord::Schema.define(version: 20161107032439) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role_id"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
